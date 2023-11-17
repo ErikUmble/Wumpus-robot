5946def7aac6ce3ad5ef32aa7a0df6238738fac6
Erik Umble <erikumble@gmail.com>
2023-11-17 12:07:15 -0500

    website init

--- website/app.vue
+++ website/app.vue
@@ -1,5 +1,38 @@
+
 <template>
+  <!--This specifies global layout for each page-->
   <div>
-    <NuxtWelcome />
+    <!-- Header -->
+    <header class="text-white py-4 shadow-lg">
+      <div class="container mx-auto px-3">
+        <!-- Navigation Links -->
+        <nav class="flex items-center">
+          <ul class="flex space-x-4">
+            <li>
+              <nuxt-link to="/" class="text-white hover:text-gray-300">Home</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/about" class="text-white hover:text-gray-300">About</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/blog" class="text-white hover:text-gray-300">Blog</nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/demo" class="text-white hover:text-gray-300">Demo</nuxt-link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+    <div>
+      <!--Each page template automatically goes here-->
+      <NuxtPage />
+    </div>
   </div>
 </template>
+
+<script>
+
+</script>
+
---
+++ website/tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./components/**/*.{js,vue,ts}",
+      "./layouts/**/*.vue",
+      "./pages/**/*.vue",
+      "./plugins/**/*.{js,ts}",
+      "./app.vue",
+      "./error.vue",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+      themes: [
+        "dim",
+        {
+          custom: {
+            primary: "#4169e1",
+            secondary: "#6e5393",
+            accent: "#617dff",
+            neutral: "#6b7280",
+            "base-100": "#374151",
+            info: "#a77edf",
+            success: "#36d399",
+            warning: "#fbbd23",
+            error: "#f87272",
+          },
+        },
+        
+      ],
+    },
+  };
+  
