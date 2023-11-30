81c19b96391dcfb328158a3e4ac175645a34a876
Joel McCandless <mail@joelmccandless.com>
2023-11-30 14:34:00 -0500

    Add the basic LED blink Arduino file

---
+++ arduino_code/main.ino
@@ -0,0 +1,17 @@
+int motorPin1 = 9;
+int motorPin2 = 10;
+int ledPin = 13;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
