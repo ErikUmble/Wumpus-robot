480c41c7531bc1dec305044ae43c0a3d9392b885
Erik Umble <erikumble@gmail.com>
2023-11-30 16:28:09 -0500

    add initial logs

--- website/pages/logs.vue
+++ website/pages/logs.vue
@@ -1,3 +1,12 @@
 <template>
-    
+    <div v-for="log, idx in meeting_logs" :key="idx" class="container w-full mx-2">
+        <h1 class="text-lg text-white">{{ log.date }}</h1>
+        <p>{{ log.text }}</p>
+        <br/>
+    </div>
 </template>
+
+<script setup>
+import meeting_logs from "@/assets/data/meeting_logs.json"
+
+</script>
