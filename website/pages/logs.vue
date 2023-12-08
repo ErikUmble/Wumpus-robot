ea9444bd53f4c3d1bfffd7672ae15a93d52132fd
Erik Umble <erikumble@gmail.com>
2023-12-08 14:56:25 -0500

    add styling to logs page

--- website/pages/logs.vue
+++ website/pages/logs.vue
@@ -1,10 +1,15 @@
 <template>
-    <div v-for="log, idx in meeting_logs" :key="idx" class="container w-full mx-2">
-        <h1 class="text-lg text-white">{{ log.date }}</h1>
+    <PageTitle>
+        <template v-slot:title>Meeting Logs</template>
+        <template v-slot:subtitle>The when and what of our project steps</template>
+      </PageTitle>
+    <div class="p-4 grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div v-for="(log, idx) in meeting_logs" :key="idx" class="bg-gray-800 rounded-lg p-4">
+        <h1 class="text-lg font-semibold text-white mb-2 text-center">{{ log.date }}</h1>
         <p>{{ log.text }}</p>
-        <br/>
+      </div>
     </div>
-</template>
+  </template>
 
 <script setup>
 import meeting_logs from "@/assets/data/meeting_logs.json"
