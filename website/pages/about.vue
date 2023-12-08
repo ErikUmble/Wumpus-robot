c6857c09c598690791de9935bced469f68e30ac7
Joel McCandless <mail@joelmccandless.com>
2023-12-08 18:20:11 -0500

    update about page

--- website/pages/about.vue
+++ website/pages/about.vue
@@ -12,15 +12,9 @@
     <section class="mb-8">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
             <div>
-                <h2 class="text-3xl font-semibold mb-4 text-white">The Situation...</h2>
-                    <p>NanoBot wakes up in the entrance of dark cave with 16 chambers (arranged in a 4x4 grid). Its one goal in life is to retrieve the gold that resides in one 
-                    of these chambers and return to its starting point without harm. But a Wupus, in one of the chambers, threatens to destroy NanoBot should it step 
-                    in the Wumpus' chamber. Not only that, but the cave is infested with pits that do not allow anyone who enters to leave.
-                    Each chamber has a doorway to the 4 adjacent chambers through which NanoBot can pass through, and also through which NanoBot can smell
-                    sense whatever is on the other side. If NanoBot is in a chamber adjacent to a pit, it feels a breeze, if next to the Wumpus, it smells a stench,
-                    and if adjacent to the gold, it sees a glitter. Note that it cannot tell which direction the scent is coming from, and it can receive 
-                    multiple senses at once (if adjacent to multiple non-empty chambers). NanoBot can shoot its single arrow
-                    and if it is into the chamber with the Wumpus, the Wumpus will die and the chamber becomes safe. 
+                <h2 class="text-3xl font-semibold mb-4 text-white">The Situation</h2>
+                    <p>
+		    NanoBot wakes up in the entrance of dark cave with 16 chambers (arranged in a 4x4 grid). It seeks to retrieve the gold located in one of these chambers and return to its starting point unharmed. But a Wumpus, hidden in one of the chambers, threatens to destroy NanoBot should they meet. Adding to the threat of the Wumpus, the cave is infested with inescapable pits that can trap NanoBot forever. Each chamber has doorways to its 4 adjacent chambers, which allow NanoBot to move between chambers and transmit sensory details between pits. If NanoBot is in a chamber adjacent to a pit, it feels a breeze, if next to the Wumpus, it smells a stench, and if adjacent to the gold, it sees a glitter. It can receive multiple senses at once, but cannot tell which direction the senses are coming from. Equipped with a single arrow, NanoBot can shoot its arrow into an adjacent chamber to kill the Wumpus if it is there.
                 </p>
             </div>
             <div>
@@ -36,10 +30,9 @@
             its memory of the cave and its senses.
         </p>
         <p>
-            In a more practical sense. NanoBot must navigate a 4x4 grid of black squares, separated by white lines, using IR sensors to detect
-            the boundaries. It must keep track of scents recieved by Bluetooth and logically deduce the squares that are safe to enter. 
-            It must be able to rotate, adjust for variation and discrepencies in its direction, and follow its logical path without entering a 
-            dangerous square. 
+            The actual physical challenge uses a 4x4 grid of black squares, separated by white lines, as the cave. NanoBot uses IR sensors to detect
+            the boundaries. It keeps track of scents recieved by Bluetooth and logically deduces the squares that are safe to enter. 
+            It adjusts for variation and discrepencies in its direction and position which are caused by its real (and therefore imperfect) hardware. Can NanoBot find a follow a logical path to retrieve the gold and get out safely?
         </p>
     </section>
 
