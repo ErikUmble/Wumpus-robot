8b752ae070c984ae9ac48c68f5c8eae7c1f0061b
Erik Umble <erikumble@gmail.com>
2023-12-09 08:49:57 -0500

    add content to design page and begin discussion

--- website/pages/design.vue
+++ website/pages/design.vue
@@ -1,50 +1,48 @@
 <template>
-<body class="">
+    <PageTitle>
+        <template v-slot:title>Design</template>
+        <template v-slot:subtitle>Decisions in hardware and software</template>
+    </PageTitle>
 
-<header class="py-8 px-8 bg-base">
-    <!-- Your header content goes here -->
-    <h1 class="text-4xl font-bold text-center text-white">Wumpus World Problem</h1>
-    <p class="mt-2 text-lg text-center">An AI challenge in robotic perception and logical decision-making</p>
-</header>
-
-<main class="container mx-auto py-8 px-4">
-    <!-- Your main content goes here -->
-    <section class="mb-8">
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <div>
-                <h2 class="text-3xl font-semibold mb-4 text-white">The Situation...</h2>
-                    <p>NanoBot wakes up in the entrance of dark cave with 16 chambers (arranged in a 4x4 grid). Its one goal in life is to retrieve the gold that resides in one 
-                    of these chambers and return to its starting point without harm. But a Wupus, in one of the chambers, threatens to destroy NanoBot should it step 
-                    in the Wumpus' chamber. Not only that, but the cave is infested with pits that do not allow anyone who enters to leave.
-                    Each chamber has a doorway to the 4 adjacent chambers through which NanoBot can pass through, and also through which NanoBot can smell
-                    sense whatever is on the other side. If NanoBot is in a chamber adjacent to a pit, it feels a breeze, if next to the Wumpus, it smells a stench,
-                    and if adjacent to the gold, it sees a glitter. Note that it cannot tell which direction the scent is coming from, and it can receive 
-                    multiple senses at once (if adjacent to multiple non-empty chambers). NanoBot can shoot its single arrow
-                    and if it is into the chamber with the Wumpus, the Wumpus will die and the chamber becomes safe. 
-                </p>
-            </div>
-            <div>
-                <img src="" alt="Wumpus World" class="w-full">
-            </div>
-        </div>
+    <section class="mb-8 mx-auto container">
+        <h2 class="text-3xl font-semibold mb-4 text-white">NanoBot: Hardware</h2>
+        <p>
+            TODO: add details of how the robot works physically, and design decisions made along the way
+        </p>
+        
     </section>
 
-    <section class="mb-8">
-        <h2 class="text-3xl font-semibold mb-4 text-white">The Challenge:</h2>
+    <section class="mb-8 mx-auto container py-2">
+        <h2 class="text-3xl font-semibold mb-4 text-white">NanoBot: Algorithm</h2>
         <p>
-            NanoBot must navigate the cave, aoviding the pits and the Wumpus (or shooting it with its single arrow), find the gold, and return to the start using
-            its memory of the cave and its senses.
+            At a high level, NanoBot keeps track of three things: its current state, the scents it has recieved, and the best understanding of
+            the board possible from what it has learned. 
         </p>
         <p>
-            In a more practical sense. NanoBot must navigate a 4x4 grid of black squares, separated by white lines, using IR sensors to detect
-            the boundaries. It must keep track of scents recieved by Bluetooth and logically deduce the squares that are safe to enter. 
-            It must be able to rotate, adjust for variation and discrepencies in its direction, and follow its logical path without entering a 
-            dangerous square. 
+            NanoBot needs to keep track of its current position and orientation on the board, to be able to rotate and move along particular paths. 
+            It does this by updating its internal state every time it takes an action such as moving or rotating. 
+        </p>
+        <p>
+            Similarly, it needs to keep track of the scents it has recieved, so that it can use them to make deductions about the board.
+        </p>
+        <p>
+            The main "intelligence" of NanoBot is in its ability to update its understanding of the board efficiently, so as to determine 
+            what might be in each tile, and plan a fast and safe route to each subsequent location. There are three key deductions that NanoBot makes 
+            when it recieves a new scent:
+            <ul class="list-inside list-disc mt-2 ps-5 py-2">
+                <li>1. Eliminate the possibility of any tile type not contained in the scent from adjacent positions.</li>
+                <li>2. If glitter or stench scented, eliminate the possibility of gold or Wumpus in all non adjacent squares (since there are just one of each on the map).</li>
+                <li>3. If NanoBot sensed something at a position and eliminated that option from 3 adjacent locations, then the 4th adjacent position must be the tile producing the scent</li>
+            </ul>
+            In games that are solvable, but only under the assumption that they are solvable, NanoBot uses several more complex heuristics to determine which risky location is the one that 
+            must be safe, based on the remaining positions where gold is possible and the elimination of paths that pass through 100% confirmed pits.
+        </p>
+        <p>
+            A routine opperation NanoBot needs to perform is to choose a tile to visit next and form a path to take to get there. Currently, NanoBot's metric for ranking next locations to visit is based
+            on the maximizing the expected information gained by sensing at that tile (for instance, a tile with three unknown adjacent tiles stands to provide more information than a tile with only one unknown adjacent tile),
+            and it prefers high ranked locations that are nearby, thus forming a hybrid approach between breadth first and depth first search. 
+            To compute the optimal path, NanoBot runs Djikstra's algorithm, breadth first searching outward from the target location until finding the current location, and then follows the reverse path. 
         </p>
     </section>
-
-</main>
-
-</body>
 
 </template>
--- website/pages/discussion.vue
+++ website/pages/discussion.vue
@@ -1,4 +1,9 @@
 <template>
+    <PageTitle>
+        <template v-slot:title>Discussion</template>
+        <template v-slot:subtitle>Comparing NanoBot with its competition and AI in general</template>
+    </PageTitle>
 
     
+
 </template>
