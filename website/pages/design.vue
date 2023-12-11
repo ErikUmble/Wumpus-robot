1484b237594d61b915d2e262316e25ad69377591
Joel McCandless <mail@joelmccandless.com>
2023-12-10 22:25:44 -0500

    write most of hardware section

--- website/pages/design.vue
+++ website/pages/design.vue
@@ -9,9 +9,17 @@
       <section class="mb-8 mx-auto px-6 container">
 	<h2 class="text-3xl font-semibold mb-4 text-white">Hardware</h2>
 	<p>
-	  TODO: add details of how the robot works physically, and design decisions made along the way
+	  NanoBot's "brains" come from the Arduino Nano 33 IoT. It uses 2 motors, 2 infrared sensors, and Bluetooth Low Energy (BLE) to move, sense lines, and receive sensory information, respectively.
 	</p>
-	
+	<p>
+	  The Arduino is plugged into the Rensselaer MinSeg Kit, which serves as a motor controller to control the motors safely. This kit enabled us to control the motors, but it also caused many problems for us. We studied <a src="https://cdn.shopify.com/s/files/1/0299/1013/files/MinSegNanoStick_Pinout_R3.pdf?v=1636244432">this schematic</a> carefully, but still made several mistakes using the wrong pins. For example, in our first time installing the infrared sensors, we accidentally connected them to the same pins as the motors, which caused the sensors to change based on whether or not the motors were turning, regardless of what color was beneath the sensors.
+	</p>
+	<p>
+	  We also struggled to get the sensors reporting the white lines properly. At first, we thought we had bought faulty sensors, so we used our electronic measurement kits, which students use in Electrical, Computer Systems Engineering (ECSE) classes, to reliably test the sensors. Using our trusted kits, we found that the sensors worked, but it still took some time for us to get the sensors working with the Arduino. After finding the right pins, we finally discovered that we had the sensors mounted at a suboptimal angle, as they prefer to point slightly forward rather than directly downward.
+	</p>
+	<p>
+	  Additionally, we spent a long time trying to improve the accuracy of NanoBot's movement. NanoBot will inevitably have inaccuracies because motors are not perfect and they don't simply turn a set number of rotations (and even if they did, wheels are not perfectly round, surfaces are not flawlessly flat, etc.). The reality is that any robot which interacts with its enviroment has to have a way to handle error. Part of handling error is trying to minimize it as much as possible. For minimizing the error in NanoBot's motion, we first used math to calculate the scale constant to increase power to the weaker motor in order to roughly equalize the two motors. After that, we examined other sources of inconsistencies, such as the ball pivot in the front of our robot. Our original metal ball pivot was heavy and caused unpredictable friction. So, we replaced the metal ball pivot with a plastic one. TODO: FINISH THIS SECTION!
+	</p>
       </section>
     </div>
 
