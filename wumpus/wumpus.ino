8fa44de081297ca295cafbd93e4d52c0e42e6f8f
Joel McCandless <mail@joelmccandless.com>
2023-12-11 16:52:06 -0500

    start changing wumpus.ino to use encoders and ir sensors

--- wumpus/wumpus.ino
+++ wumpus/wumpus.ino
@@ -1,11 +1,17 @@
 #include <ArduinoBLE.h>
 #include "SAMD_PWM.h"
 #include "robot.h"
+#include "Encoder.h"
 
 int motor1pin1 = 9;
 int motor1pin2 = 10;
+Encoder m1encoder(2, 3);
+
 int motor2pin1 = 6;
 int motor2pin2 = 5;
+Encoder m2encoder(11, 21);
+
+float OK_ERROR = 100;
 
 SAMD_PWM* motor1pin1pwm;
 SAMD_PWM* motor1pin2pwm;
@@ -14,20 +20,22 @@ SAMD_PWM* motor2pin2pwm;
 
 float stdFreq = 1000.0f; // operating frequency (unvarying)
 float zeroDuty = 0.0f; // off
-float slow = 18.0f; // slow speed
-float turn = 20.0f; // turn speed
+float slow = 22.0f; // slow speed
+float turnProportion = 0.05; // turn speed
+float intProportion = 0.002;
+
+int block_delay = 3600;
 
 // scale motor 2's duty cycle by a constant to adjust for differences
 // in hardware between the two motors
 float m2_scale = 1.0232;
 
-// delay needed for the bot to travel 1 block
-// when motors are all forward or all backward
-int block_delay = 3600;
+// number of encoder ticks in one block
+long blockEncTarget = 4000;
 
-// delay needed for the bot to rotate 90 degrees
-int rot_delay = 400;
-
+// number of encoder ticks in each wheel
+// to turn 90 degrees
+long turnEncTarget = 680;
 
 BLEService nanoBotService("180A"); // BLE Service
 // BLE Characteristic for transmitting scent - custom 128-bit UUID, read and writable by central
@@ -35,24 +43,78 @@ BLEByteCharacteristic scentCharacteristic("2A57", BLERead | BLEWrite);
 
 BLEDevice central;
 
+bool ir_left() {
+  // returns true if the left infrared sensor detects white
+  return analogRead(17) > 1023 / 2;
+}
+
+
+bool ir_right() {
+  // returns true if the right infrared sensor detects white
+  return analogRead(16) > 1023 / 2;
+}
+
+
 void m1Forward(float dutyCycle) {
-  motor1pin1pwm->setPWM(motor1pin1, stdFreq, dutyCycle);
-  motor1pin2pwm->setPWM(motor1pin2, stdFreq, 0.0f);
+  if (dutyCycle >= 100.0) {
+    motor1pin1pwm->setPWM(motor1pin1, stdFreq, 100.0f);
+    motor1pin2pwm->setPWM(motor1pin2, stdFreq, 0.0f);
+  }
+  else {
+    motor1pin1pwm->setPWM(motor1pin1, stdFreq, dutyCycle);
+    motor1pin2pwm->setPWM(motor1pin2, stdFreq, 0.0f);
+  }
 }
 
 void m1Backward(float dutyCycle) {
-  motor1pin1pwm->setPWM(motor2pin1, stdFreq, 0.0f);
-  motor1pin2pwm->setPWM(motor2pin2, stdFreq, dutyCycle);
+  if (dutyCycle >= 100) {
+    motor1pin1pwm->setPWM(motor1pin1, stdFreq, 0.0f);
+    motor1pin2pwm->setPWM(motor1pin2, stdFreq, 100.0f);
+  }
+  else {
+    motor1pin1pwm->setPWM(motor1pin1, stdFreq, 0.0f);
+    motor1pin2pwm->setPWM(motor1pin2, stdFreq, dutyCycle);
+  }
 }
 
 void m2Forward(float dutyCycle) {
-  motor2pin1pwm->setPWM(motor2pin1, stdFreq, dutyCycle * m2_scale);
-  motor2pin2pwm->setPWM(motor2pin2, stdFreq, 0.0f);
+  if (dutyCycle * m2_scale >= 100) {
+    motor2pin1pwm->setPWM(motor2pin1, stdFreq, 100.0);
+    motor2pin2pwm->setPWM(motor2pin2, stdFreq, 0.0f);
+  }
+  else {
+    motor2pin1pwm->setPWM(motor2pin1, stdFreq, dutyCycle * m2_scale);
+    motor2pin2pwm->setPWM(motor2pin2, stdFreq, 0.0f);
+  }
 }
 
 void m2Backward(float dutyCycle) {
-  motor2pin1pwm->setPWM(motor2pin1, stdFreq, 0.0f);
-  motor2pin2pwm->setPWM(motor2pin2, stdFreq, dutyCycle * m2_scale);
+  if (dutyCycle * m2_scale >= 100) {
+    motor2pin1pwm->setPWM(motor2pin1, stdFreq, 0.0f);
+    motor2pin2pwm->setPWM(motor2pin2, stdFreq, 100.0);
+  }
+  else {
+    motor2pin1pwm->setPWM(motor2pin1, stdFreq, 0.0f);
+    motor2pin2pwm->setPWM(motor2pin2, stdFreq, dutyCycle * m2_scale);
+  }
+}
+
+void m1SignedDirection(float dutyCycle) {
+  if (dutyCycle > 0) {
+    m1Forward(dutyCycle);
+  }
+  else {
+    m1Backward(-dutyCycle);
+  }
+}
+
+void m2SignedDirection(float dutyCycle) {
+  if (dutyCycle > 0) {
+    m2Forward(dutyCycle);
+  }
+  else {
+    m2Backward(-dutyCycle);
+  }
 }
 
 void allForward(float dutyCycle) {
@@ -69,16 +131,40 @@ void allStop() {
 }
 
 void ccw() {
-  m1Forward(turn);
-  m2Backward(turn);
-  delay(rot_delay);
+  m1encoder.readAndReset();
+  m2encoder.readAndReset();
+  float m1integral = 0;
+  float m2integral = 0;
+  while (abs(m1encoder.read() - turnEncTarget) > OK_ERROR || abs(m2encoder.read() + turnEncTarget) > OK_ERROR) {
+    if (abs(m1encoder.read() - turnEncTarget) > OK_ERROR) {
+      m1SignedDirection(-1 * (turnProportion * (m1encoder.read() - turnEncTarget) + intProportion * m1integral));
+      m1integral += m1encoder.read() - turnEncTarget;
+    }
+    if (abs(m2encoder.read() + turnEncTarget) > OK_ERROR) {
+      m2SignedDirection(-1 * (turnProportion * (m2encoder.read() + turnEncTarget) + intProportion * m2integral));
+      m2integral += m2encoder.read() + turnEncTarget;
+    }
+    delay(50);
+  }
   allStop();
 }
 
 void cw() {
-  m1Backward(turn);
-  m2Forward(turn);
-  delay(rot_delay);
+  m1encoder.readAndReset();
+  m2encoder.readAndReset();
+  float m1integral = 0;
+  float m2integral = 0;
+  while (abs(m1encoder.read() + turnEncTarget) > OK_ERROR || abs(m2encoder.read() - turnEncTarget) > OK_ERROR) {
+    if (abs(m1encoder.read() + turnEncTarget) > OK_ERROR) {
+      m1SignedDirection(-1 * (turnProportion * (m1encoder.read() + turnEncTarget) + intProportion * m1integral));
+      m1integral += m1encoder.read() + turnEncTarget;
+    }
+    if (abs(m2encoder.read() - turnEncTarget) > OK_ERROR) {
+      m2SignedDirection(-1 * (turnProportion * (m2encoder.read() - turnEncTarget) + intProportion * m2integral));
+      m2integral += m2encoder.read() - turnEncTarget;
+    }
+    delay(50);
+  }
   allStop();
 }
 
@@ -103,10 +189,28 @@ class NanoBot: public Robot {
     }
     void move_forward() {
 
-        // move NanoBot forward 1 block
+
+        // move forward and then make corrections until both sensors detect white at the same time
+        bool white_left, white_right;
+        while (true) {
+          unsigned long distance_ms = 0;
+          allForward(slow);
+          while(!ir_left() && !ir_right()) {
+            distance_ms += 1; 
+            delay(1); 
+            white_left = ir_left(); white_right = ir_right();
+            }
+          allStop();
+          if (white_left && white_right) break;
+          if (white_left) m2Backward(slow);
+          if (white_right) m1Backward(slow);
+          delay(distance_ms / 20);
+          allStop();
+        }
         allForward(slow);
-        delay(block_delay);
+        delay(block_delay/2);
         allStop();
+
 
         // update internal state
         super::move_forward();
