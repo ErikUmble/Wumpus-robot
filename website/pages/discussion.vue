a66a8b33c0d1c781fff1784627d9609380836ad1
Joel McCandless <mail@joelmccandless.com>
2023-12-13 11:58:23 -0500

    add margin top to header3

--- website/pages/discussion.vue
+++ website/pages/discussion.vue
@@ -36,21 +36,21 @@
 	  </section>
 	  <section class="mb-8">
             <h2 class="text-3xl font-semibold mb-4 text-white">Related to Class</h2>
-	    <span class="text-white text-lg">Intelligence</span>
+	    <h3 class="text-white text-lg mt-5">Intelligence</h3>
 	    <p>
 	      Intelligence is multifaceted. NanoBot displays spacial intelligence as it is able to keep track of its position and orientation and perceive its environment to make corrections to its understanding. This enabled it to generally move to its intended destination. We also think NanoBot displays logical intelligence as it is able to make deductions about the board, and navigate it in nontrivial ways. For example, NanoBot is able to able to determine where particular scents come from by comparing scents received at all nearby squares. It does not display other kinds of intelligences, so we consider NanoBot to be a weak intelligent agent. It takes in informational input (scents via Bluetooth and black/white detection via IR sensors) and processes that information to interact safely with its environment and achieve its goal of retrieving the gold.
 	    </p>
-	    <span class="text-white text-lg">Consciousness</span>
+	    <h3 class="text-white text-lg mt-5">Consciousness</h3>
 	    <p>
 	      We do not believe NanoBot is conscious, though it's impossible to be absolutely certain of this. Although it has pretty high computational speed, NanoBot is merely executing a series of instructions. Following from this, NanoBot is completely deterministic, because given the same board, it will always make identical moves. Its lack of emotional intelligence also suggests a lack of consciousness because conscious experience implies thought and desires, which typically coincide with emotions (when your desires are not satisfied, you experience negative emotion).
 	      <br />
 	      Note that we can never be completely certain of NanoBot's lack of consciousness, because consciousness is entirely internal. NanoBot could be conscious, even if it has no emotional intelligence (a purely logical being). But the evidence suggests that NanoBot is not conscious.
 	    </p>
-	    <span class="text-white text-lg">Mind-Body</span>
+	    <h3 class="text-white text-lg mt-5">Mind-Body</h3>
 	    <p>
 	      NanoBot does not seem to think, so it also does not seem to have a mind. But if it did have a mind, the mind would be the Arduino Nano's CPU and storage drives, which together store information, make logical deductions and transformations, and give instructions to the body (NanoBot's motors). This mind would be entirely materialistic, because its computation is completely credited to its physical arrangement of molecules. Following the materialist mind-body model, signals from NanoBot's infrared sensors, which are a part of its body, are sent to the mind (via wires). Analyzing NanoBot in this way, one can see how the materialist mind-body position is based on computers.
 	    </p>
-	    <span class="text-white text-lg">Artificial Intelligence</span>
+	    <h3 class="text-white text-lg mt-5">Artificial Intelligence</h3>
 	    <p>
 	      This project doesn't change our view on Artifical Intelligence (AI) that much. Every AI we've seen thus far is an algorithm that maps inputs to the same outputs. AI seems intelligent because it will give similar outputs to humans, but none of the AI is actually thinking. This project is a great example of how an algorithm can seem intelligent, because our algorithm can make all of the deductions a human can about a board, and much faster than a human can.
 	    </p>
@@ -59,7 +59,4 @@
 	    </p>
 	  </section>
 	</main>
-
-    
-
 </template>
