e66557c9abbbd62f92233c8ee85166933f2e1db8
Joel McCandless <mail@joelmccandless.com>
2023-12-13 18:03:36 -0500

    proofreading

--- website/pages/design.vue
+++ website/pages/design.vue
@@ -13,13 +13,13 @@
 	</p>
 	<img src="@/assets/images/nanobot.png" alt="NanoBot" class="w-auto bg-base" style="max-height: 80vh;">
 	<p>
-	  The Arduino is plugged into the Rensselaer MinSeg Kit, which serves as a motor controller to control the motors safely. This kit enabled us to control the motors, but it also caused many problems for us. We studied <a src="https://cdn.shopify.com/s/files/1/0299/1013/files/MinSegNanoStick_Pinout_R3.pdf?v=1636244432">this schematic</a> carefully, but still made several mistakes using the wrong pins. For example, in our first time installing the infrared sensors, we accidentally connected them to the same pins as the motors, which caused the sensors to change based on whether or not the motors were turning, regardless of what color was beneath the sensors.
+	  The Arduino is inserted into the Rensselaer MinSeg Kit, which serves as a motor controller. This kit enabled us to safely control the motors, but it also caused many problems for us. We studied <a src="https://cdn.shopify.com/s/files/1/0299/1013/files/MinSegNanoStick_Pinout_R3.pdf?v=1636244432">this schematic</a> carefully, but still made several mistakes using the wrong pins. For example, in our first time installing the infrared sensors, we accidentally connected them to the same pins as the motors, which caused the sensors to change based on whether or not the motors were turning, rather than what color was beneath the sensors.
 	</p>
 	<p>
-	  We also struggled to get the sensors reporting the white lines properly. At first, we thought we had bought faulty sensors, so we used our electronic measurement kits, which students use in Electrical, Computer Systems Engineering (ECSE) classes, to reliably test the sensors. Using our trusted kits, we found that the sensors worked, but it still took some time for us to get the sensors working with the Arduino. After finding the right pins, we finally discovered that we had the sensors mounted at a suboptimal angle, as they prefer to point slightly forward rather than directly downward.
+	  We also struggled to get the sensors reporting the white lines properly. At first, we thought we had bought faulty sensors, so we used our electronic measurement kits from our Electrical, Computer Systems Engineering (ECSE) classes to reliably test the sensors, and found that they worked! However, it took some time for us to get them working with the Arduino. After finding the right pins, we finally discovered that the sensors were very particular about their angle being downward and slightly forward.
 	</p>
 	<p>
-	  Additionally, we spent a long time trying to improve the accuracy of NanoBot's movement. NanoBot will inevitably have inaccuracies because motors are not perfect and they don't simply turn a set number of rotations (and even if they did, wheels are not perfectly round, surfaces are not flawlessly flat, etc.). The reality is that any robot which interacts with its enviroment has to have a way to handle error. Part of handling error is trying to minimize it as much as possible. For minimizing the error in NanoBot's motion, we first used math to calculate the scale constant to increase power to the weaker motor in order to roughly equalize the two motors. After that, we examined other sources of inconsistencies, such as the ball pivot in the front of our robot. Our original metal ball pivot was heavy and caused unpredictable friction. So, we replaced the metal ball pivot with a plastic one. TODO: FINISH THIS SECTION!
+	  Additionally, we spent a long time trying to improve the accuracy of NanoBot's movement. As with any robot, NanoBot will inevitably have to correct for error because motors and other electronics are not perfect (and even if they were, wheels are not perfectly round, surfaces are not flawlessly flat, etc.). To minimize our error, we first calculated the scale constant to increase power to the weaker motor in order to roughly equalize the two. After that, we examined other sources of inconsistencies, such as the ball pivot in the front of our robot. The original metal ball pivot was heavy and caused unpredictable friction. So, we replaced the metal ball pivot with a plastic one. The plastic pivot works with much lower friction, leading to smaller random error! A final change we made to improve NanoBot's movement was to add weight to the back of the robot where the wheels are, thereby increasing their friction (grip) on the floor. This prevented them from slipping even as they made small or quick adjustments.
 	</p>
 	<img src="@/assets/images/nano_pinout.webp" alt="Robot" class="w-auto m-2 py-2" style="max-height: 80vh;">
       </section>
@@ -33,8 +33,8 @@
 	  the board possible from what it has learned. 
 	</p>
 	<p>
-	  NanoBot needs to keep track of its current position and orientation on the board, to be able to rotate and move along particular paths. 
-	  It does this by updating its internal state every time it takes an action such as moving or rotating. 
+	  NanoBot needs to keep track of its current position and orientation on the board to be able to rotate and move along paths. 
+	  It does this by updating its internal state every time it takes an action such as moving or rotating.
 	</p>
 	<p>
 	  Similarly, it needs to keep track of the scents it has recieved, so that it can use them to make deductions about the board.
@@ -44,22 +44,21 @@
 	  what might be in each tile, and plan a fast and safe route to each subsequent location. There are three key deductions that NanoBot makes 
 	  when it recieves a new scent:
 	  <ul class="list-inside list-decimal mt-2 ps-5 py-2">
-            <li>Eliminate the possibility of any tile type not contained in the scent from adjacent positions.</li>
-            <li>If glitter or stench scented, eliminate the possibility of gold or Wumpus in all non adjacent squares (since there are just one of each on the map).</li>
+            <li>Eliminate from adjacent positions the possibility of any tile type not contained in the scent.</li>
+            <li>If the sense is glitter or stench, eliminate the possibility of gold or Wumpus in all non adjacent squares (since there are just one of each on the map).</li>
             <li>If NanoBot sensed something at a position and eliminated that option from 3 adjacent locations, then the 4th adjacent position must be the tile producing the scent</li>
 	  </ul>
 	  <div class="items-center justify-center">	    
 		<img src="@/assets/images/knowledge_animation.gif" alt="Knowledge Progression" class="w-auto" style="max-height: 80vh;">
 	  </div>
-	  
-	  In games that are solvable, but only under the assumption that they are solvable, NanoBot uses several more complex heuristics to determine which risky location is the one that 
-	  must be safe, based on the remaining positions where gold is possible and the elimination of paths that pass through 100% confirmed pits.
+	  In games that are solvable only with the assumption that they are solvable, NanoBot uses several more complex heuristics to determine which unknown location is the one that 
+	  must be safe, based on the remaining positions where gold is possible and the elimination of paths that pass through confirmed pits.
 	</p>
 	<p>
-	  A routine opperation NanoBot needs to perform is to choose a tile to visit next and form a path to take to get there. Currently, NanoBot's metric for ranking next locations to visit is based
+	  A routine operation NanoBot needs to perform is to choose a tile to visit next and form a path to take to get there. Currently, NanoBot's metric for ranking next locations to visit is based
 	  on the maximizing the expected information gained by sensing at that tile (for instance, a tile with three unknown adjacent tiles stands to provide more information than a tile with only one unknown adjacent tile),
 	  and it prefers high ranked locations that are nearby, thus forming a hybrid approach between breadth first and depth first search. 
-	  To compute the optimal path, NanoBot runs Djikstra's algorithm, breadth first searching outward from the target location until finding the current location, and then follows the reverse path. 
+	  To compute the optimal path, NanoBot runs Djikstra's algorithm: a breadth first searching outward from the target location until finding the current location. It then follows the reverse path. 
 	</p>
       </section>
     </div>
--- website/pages/discussion.vue
+++ website/pages/discussion.vue
@@ -15,7 +15,7 @@
 				<span class="text-center text-white text-lg">Accomplishments</span>
 				<ul class="list-disc">
 				  <li>Our line-based error correction was accurate enough to usually avoid serious accumulated error.</li>
-				  <li>Successfully completed an easy board, a medium, and two hard ones (before the battery died).</li>
+				  <li>Successfully completed an easy, medium, and two hard boards (before the battery died).</li>
 				  <li>Passed all test cases (including the hardest level) in the algorithm tests. Also demonstrated more sophisticated reasoning such as considering distance and the new information NanoBot could learn when selecting the next square to move to.</li>
 				  <li>We only shoot the Wumpus when we have to (Peace First&#x2122;)</li>
 				  <li>Our algorithm was lightweight and quick. It executed with no delay and used small storage to fit on the Arduino Nano's drives.</li>
@@ -26,7 +26,7 @@
 			  <span class="text-center text-white text-lg">Future Improvements</span>
 			  <ul class="list-disc">
 				  <li>It should be possible to maintain that level of error correction and do so without as many pauses and readjustments.</li>
-				  <li>Turning should be able to happen faster rather than correcting for so long. <span class="italic">Note: in the past, Mindstorms' software handled all of the behind-the-scenes work with encoder feedback loops to get the motors to their proper positions. We attempted to recreate this in our limited timeline, which is why our feedback loops are not as advanced.</span></li>
+				  <li>Turning should be able to happen faster rather than correcting for so long. <span class="italic">Note: in the past, Mindstorms' software handled all of the behind-the-scenes work with encoder feedback loops to get the motors to their proper positions. We attempted to recreate this in our limited time, which is why our feedback loops are not as advanced.</span></li>
 				  <li>When pathfinding, prefer to continue in the same direction. This will minimize error caused by turning.</li>
 				  <li>Complete all of the boards in fewer attempts.</li>
 			  </ul>
@@ -38,25 +38,23 @@
             <h2 class="text-3xl font-semibold mb-4 text-white">Related to Class</h2>
 	    <h3 class="text-white text-lg mt-5">Intelligence</h3>
 	    <p>
-	      Intelligence is multifaceted. NanoBot displays spacial intelligence as it is able to keep track of its position and orientation and perceive its environment to make corrections to its understanding. This enabled it to generally move to its intended destination. We also think NanoBot displays logical intelligence as it is able to make deductions about the board, and navigate it in nontrivial ways. For example, NanoBot is able to able to determine where particular scents come from by comparing scents received at all nearby squares. It does not display other kinds of intelligences, so we consider NanoBot to be a weak intelligent agent. It takes in informational input (scents via Bluetooth and black/white detection via IR sensors) and processes that information to interact safely with its environment and achieve its goal of retrieving the gold.
+	      As we learned in our group presentations, intelligence is multifaceted. NanoBot displays spacial intelligence as it is able to keep track of its position and orientation and use features of its environment to correct its understanding. Demonstrating spacial intelligence, it can usually move to its intended destination. NanoBot also displays logical intelligence as it is able to make deductions about the board, and navigate it in nontrivial ways. For example, NanoBot is able to able to determine where particular scents come from by comparing scents received at all nearby squares. It does not display other kinds of intelligences, so we consider NanoBot to be a weak intelligent agent. It takes in informational input (scents via Bluetooth and black/white detection via IR sensors) and processes that information to interact safely with its environment and achieve its goal of retrieving the gold.
 	    </p>
 	    <h3 class="text-white text-lg mt-5">Consciousness</h3>
 	    <p>
-	      We do not believe NanoBot is conscious, though it's impossible to be absolutely certain of this. Although it has pretty high computational speed, NanoBot is merely executing a series of instructions. Following from this, NanoBot is completely deterministic, because given the same board, it will always make identical moves. Its lack of emotional intelligence also suggests a lack of consciousness because conscious experience implies thought and desires, which typically coincide with emotions (when your desires are not satisfied, you experience negative emotion).
-	      <br />
-	      Note that we can never be completely certain of NanoBot's lack of consciousness, because consciousness is entirely internal. NanoBot could be conscious, even if it has no emotional intelligence (a purely logical being). But the evidence suggests that NanoBot is not conscious.
+	      We do not believe NanoBot is conscious, though it's impossible to be absolutely certain of this. Although it has pretty high computational speed, NanoBot is merely executing a series of instructions. It follows that NanoBot is completely deterministic, because given the same board, it will always make identical moves. Its lack of emotional intelligence also suggests a lack of consciousness because conscious experience implies thought and desires, which typically coincide with emotions (when your desires are not satisfied, you experience negative emotion). Note that this is not necessarily always the case.
 	    </p>
 	    <h3 class="text-white text-lg mt-5">Mind-Body</h3>
 	    <p>
-	      NanoBot does not seem to think, so it also does not seem to have a mind. But if it did have a mind, the mind would be the Arduino Nano's CPU and storage drives, which together store information, make logical deductions and transformations, and give instructions to the body (NanoBot's motors). This mind would be entirely materialistic, because its computation is completely credited to its physical arrangement of molecules. Following the materialist mind-body model, signals from NanoBot's infrared sensors, which are a part of its body, are sent to the mind (via wires). Analyzing NanoBot in this way, one can see how the materialist mind-body position is based on computers.
+	      NanoBot does not seem to think, so it also does not seem to have a mind. But if it did have a mind, the mind would be the Arduino Nano's CPU and storage drives, which together store information, make logical deductions and transformations, and give instructions to the body (NanoBot's motors). This mind would be entirely materialist, because its computation is credited to its physical arrangement of molecules. Exactly as described in the materialist model, signals from NanoBot's infrared sensors, which are a part of its body, are sent to the mind (via wires). Analyzing NanoBot in this way, one can see how the materialist view on the mind-body problem is based on computers.
 	    </p>
 	    <h3 class="text-white text-lg mt-5">Artificial Intelligence</h3>
 	    <p>
-	      This project doesn't change our view on Artifical Intelligence (AI) that much. Every AI we've seen thus far is an algorithm that maps inputs to the same outputs. AI seems intelligent because it will give similar outputs to humans, but none of the AI is actually thinking. This project is a great example of how an algorithm can seem intelligent, because our algorithm can make all of the deductions a human can about a board, and much faster than a human can.
+	      This project doesn't change our view on Artificial Intelligence (AI). Every AI we've seen thus far is an algorithm that maps inputs to outputs. AI seems intelligent because it will give similar outputs as humans do, but the AI is not actually thinking. This project is a great example of how an algorithm can seem intelligent, because our algorithm can make all of the deductions a human can about a board, and even much faster than a human can, but has no general intelligence.
 	    </p>
 	    <p>
-	      A shortcoming of AI that shows how it's not actually intelligent is how it operates only on the set of data or conditions for which it is trained. A human can learn and complete a variety of tasks in a variety of environments, but our algorithm will not work when it's applied to anything other than the Wumpus World Challenge. Even modifying the world so that there can be more than one Wumpus would cause our algorithm to fail. Historically, we've seen the same trend. We've made AIs that play chess or Go, but they can't play any other games. Even the more "sophisticated" AIs that learn language have the same problem, such as when IBM launched project Watson, attempting to repurpose the Jeopardy AI to be a digital assistant but failing miserably.
+	      A common shortcoming of AI that shows its false intelligence is how it works only on the set of data or conditions for which it is trained. A human can learn and complete a variety of tasks in a variety of environments, but our algorithm will not work when it's applied to anything other than the Wumpus World Challenge. Even modifying the world so that there can be more than one Wumpus would cause our algorithm to fail. Historically, we've seen the same trend. We've made AIs that play chess or Go, but they can't play any other games. Even the more "sophisticated" AIs that learn language have the same problem, such as when IBM launched project Watson, attempting to repurpose the Jeopardy AI to be a digital assistant but falling short.
 	    </p>
 	  </section>
-	</main>
+    </main>
 </template>
