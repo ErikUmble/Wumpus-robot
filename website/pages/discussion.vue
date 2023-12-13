666a75ea050656a3b7c701c031def1cecba482fc
Joel McCandless <mail@joelmccandless.com>
2023-12-12 23:25:05 -0500

    start structuring the discussion page

--- website/pages/discussion.vue
+++ website/pages/discussion.vue
@@ -3,6 +3,36 @@
         <template v-slot:title>Discussion</template>
         <template v-slot:subtitle>Comparing NanoBot with its competition and AI in general</template>
     </PageTitle>
+    <main class="container mx-auto py-8 px-4">
+	  <section class="mb-8">
+            <h2 class="text-3xl font-semibold mb-4 text-white">How'd We Do?</h2>
+	    <p>
+	      With the passing of the LEGO Mindstorms kits, we didn't have any competitors this year. But viewing footage from past challenges, it seems like we did pretty well overall!
+	    </p>
+	    <br />
+	    <table class="w-full">
+	      <thead>
+		<tr>
+		  <th>Accomplishments</th>
+		  <th>Improve for Next Time</th>
+		</tr>
+	      </thead>
+	      <tbody>
+		<tr>
+		  <td>
+		  </td>
+		  <td>
+		  </td>
+		</tr>
+	      </tbody>
+	    </table>
+	  </section>
+	  <section class="mb-8">
+            <h2 class="text-3xl font-semibold mb-4 text-white">Related to Class</h2>
+	    <p>
+	    </p>
+	  </section>
+	</main>
 
     
 
