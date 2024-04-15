4933dda1eca715e2ec68469e6f3a5fb788555914
Erik Umble <erikumble@gmail.com>
2024-04-15 14:28:37 -0400

    Add distance method

--- wumpus/wumpus.py
+++ wumpus/wumpus.py
@@ -406,6 +406,10 @@ class Robot:
                     max_sum = sum
                     best_pos = potential
         return best_pos
+
+    def distance(self, pos1, pos2):
+        return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
     
     def sniff(self):
         """
